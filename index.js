fc827833b1ba1d82bc40c507a3c0cd72c97cd071
Riyad Hosen <riyadhosen@Riyads-Air.netis.cc>
2024-06-10 16:22:32 -0700

    post method for users

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const app = express()
+const app = express();
 const port = process.env.PORT || 3200;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
-app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174",],
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
     optionSuccessStatus: 200,
-}))
-app.use(express.json())
+  })
+);
+app.use(express.json());
 
+const password = process.env.PASS;
 
+const uri = `mongodb+srv://class_edge:${password}@cluster0.x7zkge4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const userCollection = client.db("ClassEdge").collection("users");
 
-app.get('/', (req, res)=>{
-    res.send('ClassEdge is running')
-})
-app.listen(port, ()=>{
-    console.log('ClassEdge is running on port: ', port)
-})
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const isExist = await userCollection.findOne(query);
+        if (isExist) {
+          return res.send({ message: "already exist user" });
+        }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ClassEdge is running");
+});
+app.listen(port, () => {
+  console.log("ClassEdge is running on port: ", port);
+});
